33a7e697e67e4b0f8425210460774f1ee1898b07
gayagenius <gayaflo95@gmail.com>
2019-11-05 19:12:59 +0300

    adding files

--- AkiraChix/settings.py
+++ AkiraChix/settings.py
@@ -130,5 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'student/media')
 MEDIA_ROOT = os.path.join(BASE_DIR,'teacher/media')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
